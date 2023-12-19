637fcfac2545a5b5d7b87ca23197ec382cbebc24
pad01g <101613202+pad01g@users.noreply.github.com>
2023-12-19 10:09:01 +0000

    use interaction

--- app/settings/index.tsx
+++ app/settings/index.tsx
@@ -1,20 +1,29 @@
-import { Text, View } from 'react-native';
+import { Text, TextInput, View } from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 import { useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Page() {
   // const rootPubKey = 
-  const [selectedLanguage, setSelectedLanguage] = useState();
+  const [selectedLanguage, setSelectedLanguage] = useState("");
   // const selectedLanguage = "java";
   return <View>
+    <Text>Setting page</Text>
+    <Text>Preset:</Text>
     <Picker
       selectedValue={selectedLanguage}
-      onValueChange={(itemValue, itemIndex) =>
+      onValueChange={async (itemValue, itemIndex) => {
         setSelectedLanguage(itemValue)
-      }>
+        try {
+          await AsyncStorage.setItem('my-key', selectedLanguage);
+        } catch (e) {
+          // saving error
+        }
+      }}>
       <Picker.Item label="Java" value="java" />
       <Picker.Item label="JavaScript" value="js" />
     </Picker>
-    <Text>Setting page</Text>
+    <Text>Enter your own key:</Text>
+    <TextInput/>
   </View>;
 }
